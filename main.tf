6e9903b09dbb2287d63630e7f5c8533eb1808882
Superfly101 <Danielukoha101@gmail.com>
2025-05-23 20:32:13 +0100

    use vmss extension block

--- main.tf
+++ main.tf
@@ -72,10 +72,10 @@ resource "azurerm_windows_virtual_machine_scale_set" "vmss" {
   computer_name_prefix     = "vm"
   enable_automatic_updates = false
 
-  source_image_id = data.azurerm_image.custom.id
+  # source_image_id = data.azurerm_image.custom.id
 
   dynamic "source_image_reference" {
-    for_each = data.azurerm_image.custom.id == null ? [var.source_image] : []
+    for_each = var.custom_image_name == null ? [var.source_image] : []
     content {
       publisher = var.source_image.publisher
       offer     = var.source_image.offer
@@ -106,7 +106,6 @@ resource "azurerm_windows_virtual_machine_scale_set" "vmss" {
     }
   }
 
-  # Custom script extension to download and execute the PowerShell script
   extension {
     name                       = "CustomScriptExtension"
     publisher                  = "Microsoft.Compute"
@@ -124,4 +123,27 @@ resource "azurerm_windows_virtual_machine_scale_set" "vmss" {
       commandToExecute   = "powershell -ExecutionPolicy Unrestricted -File ${var.script_file_name} -TaskName 'DemoScheduledTask' -TaskDescription 'Demo task created via Terraform VMSS'"
     })
   }
+
 }
+
+
+
+# Custom script extension to download and execute the PowerShell script
+# resource "azurerm_virtual_machine_scale_set_extension" "example" {
+#   name                         = "CustomScriptExtension"
+#   publisher                    = "Microsoft.Compute"
+#   type                         = "CustomScriptExtension"
+#   type_handler_version         = "1.10"
+#   auto_upgrade_minor_version   = true
+#   virtual_machine_scale_set_id = azurerm_windows_virtual_machine_scale_set.vmss.id
+
+#   settings = jsonencode({
+#     fileUris = [var.script_blob_url]
+#   })
+
+#   protected_settings = jsonencode({
+#     storageAccountName = var.storage_account_name
+#     storageAccountKey  = var.storage_account_key
+#     commandToExecute   = "powershell -ExecutionPolicy Unrestricted -File ${var.script_file_name} -TaskName 'DemoScheduledTask' -TaskDescription 'Demo task created via Terraform VMSS'"
+#   })
+# }
